0ed7e2465480c3d2978eac3179dbc85fb0c15d82
Luis Laguado <luisalfonso099@gmail.com>
2023-09-07 18:57:07 -0300

    first commit

---
+++ resources/js/Pages/Posts/Create.vue
@@ -0,0 +1,32 @@
+<script setup>
+import {useForm} from '@inertiajs/vue3';
+const testfunction = ()=> {
+    my_modal_1.showModal()
+}
+const form = useForm({
+    title : ''
+})
+
+const savePost = ()=> {
+    my_modal_1.close()
+    console.log('form::',form);
+
+    form.post('/posts')
+}
+
+</script>
+
+<template>
+    <button class="btn" @click="testfunction" >New Post</button>
+    <dialog id="my_modal_1" class="modal">
+        <div class="modal-box">
+            <h3 class="font-bold text-lg">Hola crea un nuevo post 😁!</h3>
+            <!-- <form action=""> -->
+                <textarea v-model="form.title" class="textarea textarea-primary mt-12 w-full h-40" placeholder="Bio"></textarea>
+                <div class="modal-action">
+                    <button @click="savePost" class="btn  btn-primary">Save</button>
+                </div>
+            <!-- </form> -->
+        </div>
+    </dialog>
+</template>
---
+++ resources/js/Pages/Posts/Index.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Layout from '../../Layouts/AppLayout.vue'
+import { Head } from '@inertiajs/vue3'
+import Create from './Create.vue'
+
+defineProps({ posts: Array })
+// console.log('Hersoo', posts);
+</script>
+
+<template >
+  <Layout>
+    <div class="container mx-auto">
+         <!-- <Head title="Welcome" /> -->
+        <h1>Posts</h1>
+        <p>Hello {{ posts.name }}, welcome to your first Inertia app!</p>
+        <Create />
+    </div>
+  </Layout>
+</template>bn
